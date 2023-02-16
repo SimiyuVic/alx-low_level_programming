99964f2935a4e6080bdbe69bcf00abb1869767ae
SimiyuVic <charlesvictor369@gmail.com>
2023-02-16 15:27:47 -0800

    Task 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - prints exactly with proper grammar,
+ * but the outcome is a piece of art
+ * followed by a new line
+ * Returns: 0
+ * compile without warning when using the -Wall gcc option
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
